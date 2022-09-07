31caef7e252a01652e55e7047bb67014a797ae17
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-07 15:19:13 -0300

    commit

---
+++ Aulas/ex 1/script.js
@@ -0,0 +1,28 @@
+
+//TIPOS DE  CONSOLE
+  console.error("Este é um erro!")
+  console.warn("Este é um aviso!")
+  console.log("Esta é uma mensagem!")
+
+  /*
+   O console mais utilizado é o "console.log" que 
+   serve para ver se ele entrou num determinado trecho 
+   do código.
+  */
+
+   //VARIÁVEIS
+   var x = 10
+   //Boas práticas 
+   let y = 15
+   const z = 20  //palavra reservada,não tem como alterar seu valor
+
+   if (1){
+     let y = 55
+   }
+
+   console.log(x)
+   console.log(y)
+   console.log(z)
+
+   y = 25
+   console.log(y)
