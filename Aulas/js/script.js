6e0d86aa6ee1007c276cf0f54a72d17fcb4aab7f
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-11 21:28:45 -0300

    sobre json

---
+++ Aulas/js/script.js
@@ -0,0 +1,41 @@
+const objs = [
+ 
+  {
+ nome:'Gabriel',
+ idade: 17,
+ esta_trabalhando:true,
+ detallhes_profissao:{
+  profissao:'programado'
+  empresa:'empresa x'  
+ },
+ hobbies:['programar','correr','ler'],
+  },
+
+  {
+    nome:'Lucas',
+    idade: 37,
+    esta_trabalhando:false,
+    detallhes_profissao:{
+     profissao:null
+     empresa: null
+    },
+    hobbies:['jogar','academia'],
+     }
+]
+
+console.log(objs)
+//JSON
+//converteno esse código ai de cimpa para json
+const jsonData = JSON.stringify(objs)//garantir que meu json seja válido
+console.log(jsonData)
+console.log(typeof jsonData)
+
+//CONVERTENDO JSON PARA OBJETO
+const objData = JSON.parse(jsonData);
+console.log(objData)
+console.log(typeof objData) //Typeof vai mostrar qual tipo de dado é number...
+
+objData.map((pessoa) =>{
+ console.log(pessoa.nome)
+});
+
